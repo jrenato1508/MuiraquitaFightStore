7be8cbe836eff23e3e0000c876af5c74c30f368b
José Renato <jrenato1508@gmail.com>
2024-08-29 18:16:50 -0300

    ciração da class entity no core e inicio da implementação do contexto de catalogo

---
+++ src/MuiraquitaFightStore.Catalogo.Domain/Entitys/Produto.cs
@@ -0,0 +1,24 @@
+﻿using MuiraquitaFightStore.Core.DomainObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuiraquitaFightStore.Catalogo.Domain.Entitys
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public Guid CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+        public string tamanho { get; private set; }
+        public Dimensoes Dimensoes { get; private set; }
+        public Categoria Categoria { get; private set; }
+    }
+}
