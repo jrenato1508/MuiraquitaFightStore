46fb7c8838336d75122057c747ae90cb0840a84c
José Renato <jrenato1508@gmail.com>
2024-09-09 17:08:06 -0300

    iniciando a implementação de Event

---
+++ src/MuiraquitaFightStore.Catalogo.Domain/Events/ProdutoAbaixoEstoqueEvent.cs
@@ -0,0 +1,18 @@
+﻿using MuiraquitaFightStore.Core.Messages.CommonMessages.DomaninEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuiraquitaFightStore.Catalogo.Domain.Events
+{
+    public class ProdutoAbaixoEstoqueEvent : DomaninEvent
+    {
+        public int QuantidadeRestante { get; set; }
+        public ProdutoAbaixoEstoqueEvent(Guid aggregateId, int quantidadeRestante) : base(aggregateId)
+        {
+            QuantidadeRestante = quantidadeRestante;
+        }
+    }
+}
